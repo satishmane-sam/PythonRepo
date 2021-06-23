5ffc636ed03f9110f40c08bef663f751aeb331f0
Satish Mane <manes1777@gmail.com>
2021-06-23 19:53:09 +0530

    Add files via upload

---
+++ 2d_lists_and_nested_Loops.py
@@ -0,0 +1,12 @@
+number_grid = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [0]
+]
+print(number_grid[0][1])
+
+'''nested loops'''
+for row in number_grid:
+    for col in row:
+        print(col)
